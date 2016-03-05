022639cd386e374e44debea133e8e967f3f1f93d
Elena <Elena@none>
2016-03-05 16:35:57 +0200

    #2 Create home page, registration and login form (only html)

--- BookTraker/WebContent/scripts/functionalities.js
+++ BookTraker/WebContent/scripts/functionalities.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 	"use strict";
 	
-	
-});
+	function loginModal(){
+		$(document).on('click','.signup-tab',function(e){
+			e.preventDefault();
+		    $('#signup-taba').tab('show');
+		});	
+		
+		$(document).on('click','.signin-tab',function(e){
+		   	e.preventDefault();
+		    $('#signin-taba').tab('show');
+		});
+		    	
+		$(document).on('click','.forgetpass-tab',function(e){
+		 	e.preventDefault();
+		   	$('#forgetpass-taba').tab('show');
+		});
+	}
+	loginModal();
+});	
